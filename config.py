8dbde031a796ff226786a373435d538552132f0a
Yunus Ahmad <115855149+yunus25jmi1@users.noreply.github.com>
2023-03-02 17:45:31 +0530

    Update and rename sample_config.py to config.py

--- config.py
+++ config.py
@@ -12,16 +12,16 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 class Config(object):
 
     # Get a bot token from botfather
-    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "6263835773:AAHR_BB-kuAWUIlaGdr-4nOtKV5SCMjltUY")
 
     # Get from my.telegram.org
-    APP_ID = int(os.environ.get("APP_ID", ""))
+    APP_ID = int(os.environ.get("APP_ID", "7946647"))
 
     # Get from my.telegram.org
-    API_HASH = os.environ.get("API_HASH", "")
+    API_HASH = os.environ.get("API_HASH", "3d564ea96a493b54f8aa5ebb3408bb9f")
 
     # Generate a user session string
-    TG_USER_SESSION = os.environ.get("TG_USER_SESSION", "")
+    TG_USER_SESSION = os.environ.get("TG_USER_SESSION", "BQB5QZcAFLhT8lwzlK63TdMw6elQQuiRZNrNTs2qKmiVXzWQwMetrGcfHXrXP_Ud7s62Iq0AWqwAgqrFDsMnyRE0sM0n2FzW1SVAj0_35tJGdPf_pExzBGAokcblL4-_yWc7xnBd81MpXYbRTx5r1XdzUYjl6pWPyOg-KMKHdrOmWK1TZIWmp4Knr0LaainaPZMtrlpKIXMw3iaqIuqeoSQfbKxBbZR7aK0ZqC7gslBt672PnzsPBC1j9PPVwfgrJW78SiiuJA26dkO_kjvqE7QM-dZmXWVWkepAUPYG8cEpgHBx25grf9bdsXFTuFSRG9bYM43donTZfaEFuupacuMJicnntgAAAABr_tVzAA")
 
     # Database URI
     DB_URI = os.environ.get("DATABASE_URL", "")
